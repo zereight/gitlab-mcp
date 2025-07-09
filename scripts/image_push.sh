d44557ba6308ce8e07386d4bcde7bacb61eb7b6f
iwakitakuma <39580206+iwakitakuma33@users.noreply.github.com>
2025-07-09 19:35:17 +0900

    FIX: push package
    issue: #140

--- scripts/image_push.sh
+++ scripts/image_push.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+git fetch
 
 if [ -z "$1" ]; then
   echo "Error: docker user name required."
   exit 1
 fi
 
+git checkout main
+
 DOCKER_USER=$1
 IMAGE_NAME=gitlab-mcp
-IMAGE_VERSION=$(jq -r '.version' package.json)
+IMAGE_VERSION=$(git describe --tags --abbrev=0)
 
+git checkout "${IMAGE_VERSION}"
 echo "${DOCKER_USER}/${IMAGE_NAME}:${IMAGE_VERSION}"
-
 docker buildx build --platform linux/arm64,linux/amd64 \
   -t "${DOCKER_USER}/${IMAGE_NAME}:latest" \
   -t "${DOCKER_USER}/${IMAGE_NAME}:${IMAGE_VERSION}" \
