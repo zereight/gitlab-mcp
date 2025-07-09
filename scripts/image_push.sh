25d18a2e2f041dedd46f0900034c3b541171966a
iwakitakuma <iwakitakuma33@gmail.com>
2025-07-09 19:42:35 +0900

    FIX

--- scripts/image_push.sh
+++ scripts/image_push.sh
@@ -11,8 +11,8 @@ git checkout main
 DOCKER_USER=$1
 IMAGE_NAME=gitlab-mcp
 IMAGE_VERSION=$(git describe --tags --abbrev=0)
-
 git checkout "${IMAGE_VERSION}"
+IMAGE_VERSION=${IMAGE_VERSION#v}
 echo "${DOCKER_USER}/${IMAGE_NAME}:${IMAGE_VERSION}"
 docker buildx build --platform linux/arm64,linux/amd64 \
   -t "${DOCKER_USER}/${IMAGE_NAME}:latest" \
