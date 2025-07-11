429f39700ee9f48f65af8bbc8d8756abe3da37bd
iwakitakuma <iwakitakuma33@gmail.com>
2025-07-11 13:26:18 +0900

    FIX: string or number
    issue: #153

--- customSchemas.ts
+++ customSchemas.ts
@@ -7,21 +7,72 @@ export const flexibleBoolean = z.preprocess((val) => {
     return val;
 }, z.boolean());
   
-export const numericStringSchema = z.string().or(z.number()).transform((val, ctx) => {
-    let strValue: string;
-    if (typeof val === 'number') {
-      strValue = String(val);
-    } else {
-      strValue = val;
-    }
-    if (strValue.trim() === '' || isNaN(Number(strValue))) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: `Expected a numeric string or number, but received "${val}"`,
-      });
-      return z.NEVER;
-    }
-    return strValue;
+
+export const numericStringSchemaNullable = z.coerce.string().or(z.number()).transform((val, ctx) => {
+  const strValue = String(val); 
+  const trimmedStrValue = strValue.trim();
+
+  if (trimmedStrValue === '') {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: `Expected a numeric string or number, but received an empty string or string with only whitespace.`,
+    });
+    return z.NEVER;
+  }
+  if (trimmedStrValue.toLowerCase() === 'null') {
+      return null;
+  }
+  if (isNaN(Number(trimmedStrValue))) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: `Expected a numeric string or number, but received ${trimmedStrValue}`,
+    });
+    return z.NEVER;
+  }
+  return String(trimmedStrValue);
+}).nullable();
+  
+import { pino } from 'pino';
+
+const logger = pino({
+  level: process.env.LOG_LEVEL || 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      levelFirst: true,
+      destination: 2,
+    },
+  },
 });
+
   
-  
+  export const numericStringSchema = z.coerce.string().transform((val, ctx) => {
+  logger.info(`st ${val}`)
+  const strValue = String(val); 
+  const trimmedStrValue = strValue.trim();
+
+  if (trimmedStrValue === '') {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: `Expected a numeric string or number, but received an empty string or string with only whitespace.`,
+    });
+    return z.NEVER;
+  }
+  if (trimmedStrValue.toLowerCase() === 'null') {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: `Expected a numeric string or number, but received an null as string`,
+    });
+    return z.NEVER;
+  }
+  if (isNaN(Number(trimmedStrValue))) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: `Expected a numeric string or number, but received ${trimmedStrValue}`,
+    });
+    return z.NEVER;
+  }
+  logger.info(`hiii ${trimmedStrValue}`)
+  return String(trimmedStrValue);
+});
