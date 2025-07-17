21d7f8ae8005aad5c56e225b96fdbdc98432a01e
iwakitakuma <39580206+iwakitakuma33@users.noreply.github.com>
2025-07-17 14:39:34 +0900

    Bump version to 1.0.74

--- scripts/npm_push.sh
+++ scripts/npm_push.sh
@@ -2,7 +2,7 @@
 git fetch
 
 git checkout main
-IMAGE_VERSION=$(git describe --tags --abbrev=0)
+IMAGE_VERSION=$(git describe --tags --abbrev=0 --match "v[0-9]*.[0-9]*.[0-9]*" main)
 
 git checkout "${IMAGE_VERSION}"
 echo "${IMAGE_VERSION}"
