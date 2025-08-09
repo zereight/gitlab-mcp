19b725447acfbc35d4c6ae3d6f07de7df6d5d0c8
iwakitakuma <test_admin@example.com>
2025-08-09 10:22:49 +0900

    FIX

--- customSchemas.ts
+++ customSchemas.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
-import { pino } from "pino";
+import { pino } from 'pino';
 const DEFAULT_NULL = process.env.DEFAULT_NULL === "true";
 
+
 const logger = pino({
-  level: process.env.LOG_LEVEL || "info",
+  level: process.env.LOG_LEVEL || 'info',
   transport: {
-    target: "pino-pretty",
+    target: 'pino-pretty',
     options: {
       colorize: true,
       levelFirst: true,
@@ -24,9 +25,7 @@ export const flexibleBoolean = z.preprocess(val => {
   } catch {
     return false;
   }
-  return result in ["true", "t", "1"];
+  return ["true", "t", "1"].includes(result);
 }, z.boolean());
 
-export const flexibleBooleanNullable = DEFAULT_NULL
-  ? flexibleBoolean.nullable().default(null)
-  : flexibleBoolean.nullable();
+export const flexibleBooleanNullable = DEFAULT_NULL ? flexibleBoolean.nullable().default(null) : flexibleBoolean.nullable();
