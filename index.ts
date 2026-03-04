9727d097c64962033bcda651c79621eb40f91d1a
pacifical <pacifical@users.noreply.github.com>
2026-03-04 15:34:10 +0300

    fix: remove ref from group and global scopes, simplify runtime schema validation

--- index.ts
+++ index.ts
@@ -3492,7 +3492,7 @@ async function searchBlobs(params: {
 
   await handleGitLabError(response);
 
-  const data = (await response.json()) as unknown[];
+  const data = await response.json();
   return z.array(GitLabSearchBlobResultSchema).parse(data);
 }
 
@@ -7084,7 +7084,6 @@ async function handleToolCall(params: any) {
         const args = SearchCodeSchema.parse(params.arguments);
         const results = await searchBlobs({
           search: args.search,
-          ref: args.ref,
           filename: args.filename,
           path: args.path,
           extension: args.extension,
@@ -7118,7 +7117,6 @@ async function handleToolCall(params: any) {
         const results = await searchBlobs({
           search: args.search,
           group_id: args.group_id,
-          ref: args.ref,
           filename: args.filename,
           path: args.path,
           extension: args.extension,
